ba63c1b8da8e276e79168fb62f9bbe5306daae10
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-11-08 04:02:52 +0600

    get services data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,9 +18,48 @@ async function run() {
     try {
         const services = client.db('assignment-11').collection('services');
         const reviews = client.db('assignment-11').collection('reviews');
+        app.get('/service', async (req, res) => {
+
+            try {
+                const service = await services.find({}).limit(3).toArray();
+                res.send({ status: true, data: service })
+            }
+            catch {
+                res.send({ status: false, error: 'couldnt data' })
+            }
+        })
+        app.get('/services', async (req, res) => {
+
+            try {
+                const service = await services.find({}).toArray();
+                res.send({ status: true, data: service })
+            }
+            catch {
+                res.send({ status: false, error: 'couldnt data' })
+            }
+        })
+        app.get('/services/:id', async (req, res) => {
+
+            try {
+                const id = req.params.id;
+
+
+
+                const serviceOne = await services.findOne({ _id: ObjectId(id) });
+                res.send({ status: true, data: serviceOne })
+            }
+            catch {
+                res.send({ status: false, error: 'couldnt data' })
+            }
+        })
+
+
+
+        console.log(ser)
 
     }
     catch {
+
 
     }
 }
