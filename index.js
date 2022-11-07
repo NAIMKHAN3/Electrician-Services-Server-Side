79ecfd6793839a682ade5e208acfbe63884e4081
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-11-08 00:01:35 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujhfrio.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const services = client.db('assignment-11').collection('services');
+        const reviews = client.db('assignment-11').collection('reviews');
+
+    }
+    catch {
+
+    }
+}
+run().catch(e => console.log(e))
 
 
 app.listen(port, () => {
